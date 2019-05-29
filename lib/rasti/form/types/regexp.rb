3111e274c921a630510a86f6434396b353c3f36d
Gabriel Naiman <gnaiman@keepcon.com>
2019-05-29 10:20:21 -0300

    Change formatted string type to String[/[a-z]/]. Added Regexp type to validate regular expressions.

--- lib/rasti/form/types/regexp.rb
+++ lib/rasti/form/types/regexp.rb
@@ -2,26 +2,21 @@ module Rasti
   class Form
     module Types
       class Regexp
+        class << self
 
-        include Formatable
+          include Castable
 
-        def self.[](format)
-          new format
+          private
+
+          def valid?(value)
+            value.is_a?(::Regexp) || value.is_a?(::String)
+          end
+
+          def transform(value)
+            value.is_a?(::Regexp) ? value.source : ::Regexp.compile(value).source
+          end
+
         end
-
-        def to_s
-          "#{self.class}[#{format.inspect}]"
-        end
-        alias_method :inspect, :to_s
-
-        private
-        
-        attr_reader :format
-
-        def initialize(format)
-          @format = format.is_a?(String) ? ::Regexp.new(format) : format
-        end
-
       end
     end
   end
--- lib/rasti/form/types/string.rb
+++ lib/rasti/form/types/string.rb
@@ -2,10 +2,17 @@ module Rasti
   class Form
     module Types
       class String
+
+        include Formatable
+
         class << self
 
           include Castable
-        
+
+          def [](format)
+            new format
+          end
+
           private
 
           def valid?(value)
@@ -17,6 +24,20 @@ module Rasti
           end
 
         end
+
+        def to_s
+          "#{self.class}[#{format.inspect}]"
+        end
+        alias_method :inspect, :to_s
+
+        private
+        
+        attr_reader :format
+
+        def initialize(format)
+          @format = format.is_a?(String) ? ::Regexp.new(format) : format
+        end
+
       end
     end
   end
